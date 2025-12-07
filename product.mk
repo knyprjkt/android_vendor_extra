bdc73faa77492cde06e3b5991c9c5152681b3dab
angelomds42 <angelomds42@gmail.com>
2025-12-07 11:50:23 -0300

    Unify keybox configs in CertifiedPropsOverlay
    Change-Id: Ib75c06b10e8cb2ed0174f44aa89e3cb879cc73cd
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- product.mk
+++ product.mk
@@ -3,5 +3,4 @@ PRODUCT_COPY_FILES += \
     vendor/extra/config-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/overlay/config/config.xml
 
 PRODUCT_PACKAGES += \
-    CertifiedPropsOverlay \
-    KeyBoxOverlay
+    CertifiedPropsOverlay
